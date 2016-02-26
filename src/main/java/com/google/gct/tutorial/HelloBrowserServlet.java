4d9168d1b1019bd55003e08af95695a6181183be
Andras Kerekes <andras.kerekes.work@gmail.com>
2016-02-26 12:33:13 -0500

    test commit

--- src/main/java/com/google/gct/tutorial/HelloBrowserServlet.java
+++ src/main/java/com/google/gct/tutorial/HelloBrowserServlet.java
@@ -26,7 +26,7 @@ public class HelloBrowserServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     String userAgent = request.getHeader("User-Agent");
-    String displayName = "Browser";
+    String displayName = "Bro";
     
     if (userAgent != null) {
       if (userAgent.contains("Explorer")) {
